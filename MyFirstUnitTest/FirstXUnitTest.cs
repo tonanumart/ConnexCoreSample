fade2bef49339afb79fc8e69491dca1325a4daa6
Anumart <anumart@kritsakorns-MacBook-Pro.local>
2017-04-12 13:32:11 +0700

    Add Unit Test to Project

---
+++ MyFirstUnitTest/FirstXUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+namespace MyFirstUnitTest
+{
+    public class FirstXUnitTest
+    {
+        [Fact]
+        public void PasingSampleTest()
+        {
+            Assert.Equal(4,2+2);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5,2+2);
+        }
+
+    }
+}
